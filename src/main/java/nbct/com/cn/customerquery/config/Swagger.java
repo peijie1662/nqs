9c594e0bd7da509711519546b4212b9b52c87c26
kiven <kivensun@gmail.com>
2019-08-03 10:49:49 +0800

    idea  and add gitignore

--- src/main/java/nbct/com/cn/customerquery/config/Swagger.java
+++ src/main/java/nbct/com/cn/customerquery/config/Swagger.java
@@ -22,34 +22,34 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 /**
  * Swagger配置<br>
  * http://localhost/swagger-ui.html#!/
- * 
+ *
  * @author PJ
  * @version 创建时间：2018年11月19日 上午11:04:53
  */
 @Configuration
 @EnableSwagger2
 public class Swagger {
-	@Bean
-	public Docket api() {
-		ParameterBuilder tokenPar = new ParameterBuilder();
-    	List<Parameter> pars = new ArrayList<Parameter>();
-    	tokenPar.name("token").description("令牌").modelRef(new ModelRef("string")).parameterType("header").required(false).build();
-    	pars.add(tokenPar.build());
-		
-		return new Docket(DocumentationType.SWAGGER_2) //
-				.apiInfo(apiInfo()) //
-				.useDefaultResponseMessages(false) //
-				.select() //
-				.apis(RequestHandlerSelectors.any()) //
-				.paths(PathSelectors.regex("^(?!auth).*$")) //
-				.paths(Predicates.not(PathSelectors.regex("/error.*"))) //
-				.build()
-				.globalOperationParameters(pars);
-	}
+    @Bean
+    public Docket api() {
+        ParameterBuilder tokenPar = new ParameterBuilder();
+        List<Parameter> pars = new ArrayList<Parameter>();
+        tokenPar.name("token").description("令牌").modelRef(new ModelRef("string")).parameterType("header").required(false).build();
+        pars.add(tokenPar.build());
 
-	private ApiInfo apiInfo() {
-		return new ApiInfoBuilder().title("NBCT Web Query Service(NQS)").description("NBCT网站查询服务").version("0.1")
-				.build();
-	}
+        return new Docket(DocumentationType.SWAGGER_2) //
+                .apiInfo(apiInfo()) //
+                .useDefaultResponseMessages(false) //
+                .select() //
+                .apis(RequestHandlerSelectors.any()) //
+                .paths(PathSelectors.regex("^(?!auth).*$")) //
+                .paths(Predicates.not(PathSelectors.regex("/error.*"))) //
+                .build()
+                .globalOperationParameters(pars);
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("NBCT Web Query Service(NQS)").description("NBCT网站查询服务").version("0.1")
+                .build();
+    }
 
 }
