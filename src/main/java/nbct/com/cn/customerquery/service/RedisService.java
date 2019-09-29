d2dcaeb3f1ea4f79d261b8d7de8e4b189537ef4f
peijie <pjwqq@sohu.com>
2019-09-29 09:46:03 +0800

    0929

--- src/main/java/nbct/com/cn/customerquery/service/RedisService.java
+++ src/main/java/nbct/com/cn/customerquery/service/RedisService.java
@@ -19,7 +19,6 @@ import org.springframework.stereotype.Component;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 
-import nbct.com.cn.customerquery.controller.Login;
 import nbct.com.cn.customerquery.entity.satistics.CallSatistics;
 
 /**
