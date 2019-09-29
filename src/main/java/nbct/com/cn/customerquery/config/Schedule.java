65e756c905122a26301570a9ee3705d0aa4cc806
peijie <pjwqq@sohu.com>
2019-09-29 14:49:25 +0800

    0929

--- src/main/java/nbct/com/cn/customerquery/config/Schedule.java
+++ src/main/java/nbct/com/cn/customerquery/config/Schedule.java
@@ -3,6 +3,8 @@ package nbct.com.cn.customerquery.config;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -11,13 +13,16 @@ import org.springframework.stereotype.Component;
 import nbct.com.cn.customerquery.mapper.as400dl.As400dlCirculationTaskMapper;
 import nbct.com.cn.customerquery.mapper.qry.QryCirculationTaskMapper;
 import nbct.com.cn.customerquery.mapper.web.WebCirculationTaskMapper;
+import nbct.com.cn.customerquery.service.RedisService;
 
 /**
  * 定时任务
  */
 @Component
 @EnableScheduling
-public class Schedle {
+public class Schedule {
+	
+	private static final Logger logger = LoggerFactory.getLogger(RedisService.class);
 
 	@Autowired
 	private As400dlCirculationTaskMapper asMapper;
@@ -36,7 +41,6 @@ public class Schedle {
 		asMapper.idleCall();
 		qryMapper.idleCall();
 		webMapper.idleCall();
-
-		System.out.println("IdleCall:" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+		logger.info("Idle DB Call:" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
 	}
 }
