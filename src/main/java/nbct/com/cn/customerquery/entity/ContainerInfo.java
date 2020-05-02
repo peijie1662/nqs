71dd05dd73e21257898aa3aad68654abaa62adc1
kiven <kivensun@gmail.com>
2020-05-02 08:28:49 +0800

    Cancel curvs JsonIgnore

--- src/main/java/nbct/com/cn/customerquery/entity/ContainerInfo.java
+++ src/main/java/nbct/com/cn/customerquery/entity/ContainerInfo.java
@@ -9,341 +9,334 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
  * @version 创建时间：2019年7月22日 下午3:16:46
  */
 public class ContainerInfo {
-	
-	private String cntrId;
-	
-	private String lncd;
 
-	private String ctsz;
-	
-	private String ctty;
-	
-	private String imvsvy;
-	
-	private String exvsvy;
-	
-	@JsonIgnore
-	private String curvscd;
-	
-	@JsonIgnore
-	private String curvsvy;
-	
-	@JsonIgnore
-	private String curvsdr;
-	
-	private String intd;
-	
-	private String infe;
-	
-	private String inog;
-	
-	private String inth;
-	
-	private String inhz;
-	
-	private String indm;
-	
-	/**
-	 * 进场时间
-	 */
-	private String inYardDt;
-	
-	/**
-	 * 出场时间
-	 */
-	private String outYardDt;
-	
-	/**
-	 * 进港类型
-	 */
-	private String inPortType;
-	
-	/**
-	 * 装卸港
-	 */
-	private String port;
-	
-	/**
-	 * 堆存天数
-	 */
-	private String storageDays;
-	
-	/**
-	 * 输单重量
-	 */
-	private String weight;
-	
-	/**
-	 * 是否装船
-	 */
-	private String loaded;
-	
-	/**
-	 * 海关放行
-	 */
-	private String customRelease;
-	
-	/**
-	 * 码头放行
-	 */
-	private String terminalRelease;
-	
-	/**
-	 * 提单号
-	 */
-	private String cabl;
-	
-	private String caag;
-	
-	/**
-	 * VGM称重方式
-	 */
-	private String weighingWayVGM;
-	
-	/**
-	 * VGM重量
-	 */
-	private String weightVGM;
-	
-	/**
-	 * VGM签名
-	 */
-	private String signVGM;
+  private String cntrId;
 
-	public String getCntrId() {
-		return cntrId;
-	}
+  private String lncd;
 
-	public void setCntrId(String cntrId) {
-		this.cntrId = cntrId;
-	}
+  private String ctsz;
 
-	public String getLncd() {
-		return lncd;
-	}
+  private String ctty;
 
-	public void setLncd(String lncd) {
-		this.lncd = lncd;
-	}
+  private String imvsvy;
 
-	public String getCtsz() {
-		return ctsz;
-	}
+  private String exvsvy;
 
-	public void setCtsz(String ctsz) {
-		this.ctsz = ctsz;
-	}
+  private String curvscd;
 
-	public String getCtty() {
-		return ctty;
-	}
+  private String curvsvy;
 
-	public void setCtty(String ctty) {
-		this.ctty = ctty;
-	}
+  private String curvsdr;
 
-	public String getImvsvy() {
-		return imvsvy;
-	}
+  private String intd;
 
-	public void setImvsvy(String imvsvy) {
-		this.imvsvy = imvsvy;
-	}
+  private String infe;
 
-	public String getExvsvy() {
-		return exvsvy;
-	}
+  private String inog;
 
-	public void setExvsvy(String exvsvy) {
-		this.exvsvy = exvsvy;
-	}
+  private String inth;
 
-	public String getIntd() {
-		return intd;
-	}
+  private String inhz;
 
-	public void setIntd(String intd) {
-		this.intd = intd;
-	}
+  private String indm;
 
-	public String getInfe() {
-		return infe;
-	}
+  /**
+   * 进场时间
+   */
+  private String inYardDt;
 
-	public void setInfe(String infe) {
-		this.infe = infe;
-	}
+  /**
+   * 出场时间
+   */
+  private String outYardDt;
 
-	public String getInog() {
-		return inog;
-	}
+  /**
+   * 进港类型
+   */
+  private String inPortType;
 
-	public void setInog(String inog) {
-		this.inog = inog;
-	}
+  /**
+   * 装卸港
+   */
+  private String port;
 
-	public String getInth() {
-		return inth;
-	}
+  /**
+   * 堆存天数
+   */
+  private String storageDays;
 
-	public void setInth(String inth) {
-		this.inth = inth;
-	}
+  /**
+   * 输单重量
+   */
+  private String weight;
 
-	public String getInhz() {
-		return inhz;
-	}
+  /**
+   * 是否装船
+   */
+  private String loaded;
 
-	public void setInhz(String inhz) {
-		this.inhz = inhz;
-	}
+  /**
+   * 海关放行
+   */
+  private String customRelease;
 
-	public String getIndm() {
-		return indm;
-	}
+  /**
+   * 码头放行
+   */
+  private String terminalRelease;
 
-	public void setIndm(String indm) {
-		this.indm = indm;
-	}
+  /**
+   * 提单号
+   */
+  private String cabl;
 
-	public String getInYardDt() {
-		return inYardDt;
-	}
+  private String caag;
 
-	public void setInYardDt(String inYardDt) {
-		this.inYardDt = inYardDt;
-	}
+  /**
+   * VGM称重方式
+   */
+  private String weighingWayVGM;
 
-	public String getOutYardDt() {
-		return outYardDt;
-	}
+  /**
+   * VGM重量
+   */
+  private String weightVGM;
 
-	public void setOutYardDt(String outYardDt) {
-		this.outYardDt = outYardDt;
-	}
+  /**
+   * VGM签名
+   */
+  private String signVGM;
 
-	public String getInPortType() {
-		return inPortType;
-	}
+  public String getCntrId() {
+    return cntrId;
+  }
 
-	public void setInPortType(String inPortType) {
-		this.inPortType = inPortType;
-	}
+  public void setCntrId(String cntrId) {
+    this.cntrId = cntrId;
+  }
 
-	public String getPort() {
-		return port;
-	}
+  public String getLncd() {
+    return lncd;
+  }
 
-	public void setPort(String port) {
-		this.port = port;
-	}
+  public void setLncd(String lncd) {
+    this.lncd = lncd;
+  }
 
-	public String getStorageDays() {
-		return storageDays;
-	}
+  public String getCtsz() {
+    return ctsz;
+  }
 
-	public void setStorageDays(String storageDays) {
-		this.storageDays = storageDays;
-	}
+  public void setCtsz(String ctsz) {
+    this.ctsz = ctsz;
+  }
 
-	public String getWeight() {
-		return weight;
-	}
+  public String getCtty() {
+    return ctty;
+  }
 
-	public void setWeight(String weight) {
-		this.weight = weight;
-	}
+  public void setCtty(String ctty) {
+    this.ctty = ctty;
+  }
 
-	public String getLoaded() {
-		return loaded;
-	}
+  public String getImvsvy() {
+    return imvsvy;
+  }
 
-	public void setLoaded(String loaded) {
-		this.loaded = loaded;
-	}
+  public void setImvsvy(String imvsvy) {
+    this.imvsvy = imvsvy;
+  }
 
-	public String getCustomRelease() {
-		return customRelease;
-	}
+  public String getExvsvy() {
+    return exvsvy;
+  }
 
-	public void setCustomRelease(String customRelease) {
-		this.customRelease = customRelease;
-	}
+  public void setExvsvy(String exvsvy) {
+    this.exvsvy = exvsvy;
+  }
 
-	public String getTerminalRelease() {
-		return terminalRelease;
-	}
+  public String getIntd() {
+    return intd;
+  }
 
-	public void setTerminalRelease(String terminalRelease) {
-		this.terminalRelease = terminalRelease;
-	}
+  public void setIntd(String intd) {
+    this.intd = intd;
+  }
 
-	public String getCabl() {
-		return cabl;
-	}
+  public String getInfe() {
+    return infe;
+  }
 
-	public void setCabl(String cabl) {
-		this.cabl = cabl;
-	}
+  public void setInfe(String infe) {
+    this.infe = infe;
+  }
 
-	public String getCaag() {
-		return caag;
-	}
+  public String getInog() {
+    return inog;
+  }
 
-	public void setCaag(String caag) {
-		this.caag = caag;
-	}
+  public void setInog(String inog) {
+    this.inog = inog;
+  }
 
-	public String getWeightVGM() {
-		return weightVGM;
-	}
+  public String getInth() {
+    return inth;
+  }
 
-	public void setWeightVGM(String weightVGM) {
-		this.weightVGM = weightVGM;
-	}
+  public void setInth(String inth) {
+    this.inth = inth;
+  }
 
-	public String getSignVGM() {
-		return signVGM;
-	}
+  public String getInhz() {
+    return inhz;
+  }
 
-	public void setSignVGM(String signVGM) {
-		this.signVGM = signVGM;
-	}
+  public void setInhz(String inhz) {
+    this.inhz = inhz;
+  }
 
-	public String getCurvscd() {
-		return curvscd;
-	}
+  public String getIndm() {
+    return indm;
+  }
 
-	public void setCurvscd(String curvscd) {
-		this.curvscd = curvscd;
-	}
+  public void setIndm(String indm) {
+    this.indm = indm;
+  }
 
-	public String getCurvsvy() {
-		return curvsvy;
-	}
+  public String getInYardDt() {
+    return inYardDt;
+  }
 
-	public void setCurvsvy(String curvsvy) {
-		this.curvsvy = curvsvy;
-	}
+  public void setInYardDt(String inYardDt) {
+    this.inYardDt = inYardDt;
+  }
 
-	public String getCurvsdr() {
-		return curvsdr;
-	}
+  public String getOutYardDt() {
+    return outYardDt;
+  }
 
-	public void setCurvsdr(String curvsdr) {
-		this.curvsdr = curvsdr;
-	}
+  public void setOutYardDt(String outYardDt) {
+    this.outYardDt = outYardDt;
+  }
 
-	public String getWeighingWayVGM() {
-		return weighingWayVGM;
-	}
+  public String getInPortType() {
+    return inPortType;
+  }
 
-	public void setWeighingWayVGM(String weighingWayVGM) {
-		this.weighingWayVGM = weighingWayVGM;
-	}
-	
+  public void setInPortType(String inPortType) {
+    this.inPortType = inPortType;
+  }
+
+  public String getPort() {
+    return port;
+  }
+
+  public void setPort(String port) {
+    this.port = port;
+  }
+
+  public String getStorageDays() {
+    return storageDays;
+  }
+
+  public void setStorageDays(String storageDays) {
+    this.storageDays = storageDays;
+  }
+
+  public String getWeight() {
+    return weight;
+  }
+
+  public void setWeight(String weight) {
+    this.weight = weight;
+  }
+
+  public String getLoaded() {
+    return loaded;
+  }
+
+  public void setLoaded(String loaded) {
+    this.loaded = loaded;
+  }
+
+  public String getCustomRelease() {
+    return customRelease;
+  }
+
+  public void setCustomRelease(String customRelease) {
+    this.customRelease = customRelease;
+  }
+
+  public String getTerminalRelease() {
+    return terminalRelease;
+  }
+
+  public void setTerminalRelease(String terminalRelease) {
+    this.terminalRelease = terminalRelease;
+  }
+
+  public String getCabl() {
+    return cabl;
+  }
+
+  public void setCabl(String cabl) {
+    this.cabl = cabl;
+  }
+
+  public String getCaag() {
+    return caag;
+  }
+
+  public void setCaag(String caag) {
+    this.caag = caag;
+  }
+
+  public String getWeightVGM() {
+    return weightVGM;
+  }
+
+  public void setWeightVGM(String weightVGM) {
+    this.weightVGM = weightVGM;
+  }
+
+  public String getSignVGM() {
+    return signVGM;
+  }
+
+  public void setSignVGM(String signVGM) {
+    this.signVGM = signVGM;
+  }
+
+  public String getCurvscd() {
+    return curvscd;
+  }
+
+  public void setCurvscd(String curvscd) {
+    this.curvscd = curvscd;
+  }
+
+  public String getCurvsvy() {
+    return curvsvy;
+  }
+
+  public void setCurvsvy(String curvsvy) {
+    this.curvsvy = curvsvy;
+  }
+
+  public String getCurvsdr() {
+    return curvsdr;
+  }
+
+  public void setCurvsdr(String curvsdr) {
+    this.curvsdr = curvsdr;
+  }
+
+  public String getWeighingWayVGM() {
+    return weighingWayVGM;
+  }
+
+  public void setWeighingWayVGM(String weighingWayVGM) {
+    this.weighingWayVGM = weighingWayVGM;
+  }
+
 }
-
-
-
-
